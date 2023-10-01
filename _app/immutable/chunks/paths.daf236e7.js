3ef83b8813c92b1e223244d467b896e1dc67fcd1
milanskendzicvulic <milanskendzicvulic@gmail.com>
2023-10-01 19:38:31 +0200

    Updates

---
+++ _app/immutable/chunks/paths.daf236e7.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1rtowsf)==null?void 0:s.base)??"/aion-classic-tools";var t;const o=((t=globalThis.__sveltekit_1rtowsf)==null?void 0:t.assets)??a;export{o as a,a as b};
